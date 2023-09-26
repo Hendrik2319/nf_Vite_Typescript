042c56db6ebb4c28314f39fea2b3d3fde8419788
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-09-26 15:23:17 +0200

    solved step 3

--- src/Types.ts
+++ src/Types.ts
@@ -1,5 +1,5 @@
 
-type Grade = 1 | 2 | 3 | 4 | 5 | 6 | "A" | "B" | "C" | "D" | "E" | "F"
+type Grade = 1 | 2 | 3 | 4 | 5 | 6 | "A" | "B" | "C" | "D" | "E" | "F" | undefined
 
 type Student = {
     name: string
@@ -12,14 +12,14 @@ let student1: Student = {
     name: "Anton",
     lastName: "Meier",
     age: 17,
-    certificate: [ "A",2,"F",3,1,"B",2,5 ]
+    certificate: [ "A",2,undefined,3,1,"B",undefined,5 ]
 }
 
 function printStudent( student:Student ): void {
     console.log(
         student.name+" "+student.lastName+" ("+student.age+")\r\n"+
         "=".repeat(30)+"\r\n"+
-        "Noten: "+student.certificate.join(",")
+        "Noten: "+student.certificate.map( n => n===undefined ? "*" : n ).join(",")
     )
 }
 
